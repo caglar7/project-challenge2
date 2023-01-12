684898d76d519a6396d13dd06ec8b5cdfdb9ff87
Caglar <mcaglarcnkr@gmail.com>
2023-01-13 02:57:29 +0300

    chibi idle anim from mixamo

--- Assets/[GAME]/Scripts/Pool/PoolManager.cs
+++ Assets/[GAME]/Scripts/Pool/PoolManager.cs
@@ -14,26 +14,13 @@ public class PoolManager : MonoBehaviour
     #endregion
 
     [Header("Objects For Pooling")]
-    public GameObject money;
-    public GameObject ball;
-    public GameObject effectRemove;
-    public GameObject scorePoint;
+    public GameObject cube;
 
     [Header("Pools")]
-    [HideInInspector] public PoolingPattern moneyPool;
-    [HideInInspector] public PoolingPattern ballPool;
-    [HideInInspector] public PoolingPattern effectRemovePool;
-    [HideInInspector] public PoolingPattern scorePointPool;
+    private PoolingPattern cubePool;
 
     void StartCreation()
     {
-        ballPool = new PoolingPattern(ball);
-        ballPool.FillPool(100);
 
-        effectRemovePool = new PoolingPattern(effectRemove);
-        effectRemovePool.FillPool(50);
-
-        scorePointPool = new PoolingPattern(scorePoint);
-        scorePointPool.FillPool(50);
     }
 }
