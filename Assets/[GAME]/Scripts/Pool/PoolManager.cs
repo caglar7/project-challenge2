54f657d6081424e4d213dbc26c9ea16670d9a24b
Caglar <mcaglarcnkr@gmail.com>
2023-01-13 00:03:29 +0300

    ref assets, my custom assets

---
+++ Assets/[GAME]/Scripts/Pool/PoolManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    #region Singleton
+    public static PoolManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+
+        StartCreation();
+    }
+    #endregion
+
+    [Header("Objects For Pooling")]
+    public GameObject money;
+    public GameObject ball;
+    public GameObject effectRemove;
+    public GameObject scorePoint;
+
+    [Header("Pools")]
+    [HideInInspector] public PoolingPattern moneyPool;
+    [HideInInspector] public PoolingPattern ballPool;
+    [HideInInspector] public PoolingPattern effectRemovePool;
+    [HideInInspector] public PoolingPattern scorePointPool;
+
+    void StartCreation()
+    {
+        ballPool = new PoolingPattern(ball);
+        ballPool.FillPool(100);
+
+        effectRemovePool = new PoolingPattern(effectRemove);
+        effectRemovePool.FillPool(50);
+
+        scorePointPool = new PoolingPattern(scorePoint);
+        scorePointPool.FillPool(50);
+    }
+}
---
+++ Assets/[GAME]/Scripts/Pool/PoolingPattern.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingPattern : MonoBehaviour
+{
+    private GameObject prefab;
+    private Stack<GameObject> objPool = new Stack<GameObject>();
+
+    public PoolingPattern(GameObject prefab)
+    {
+        this.prefab = prefab;
+    }
+
+    public void FillPool(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject obje = Object.Instantiate(prefab);
+            obje.hideFlags = HideFlags.HideInHierarchy;
+            AddObjToPool(obje);
+        }
+    }
+
+    public GameObject PullObjFromPool(HideFlags flags = HideFlags.None)
+    {
+        if (objPool.Count > 0)
+        {
+            GameObject obje = objPool.Pop();
+            obje.gameObject.SetActive(true);
+            obje.hideFlags = flags;
+
+            return obje;
+        }
+
+        GameObject objeIns = Object.Instantiate(prefab);
+        objeIns.hideFlags = flags;
+        return objeIns;
+    }
+
+    public void AddObjToPool(GameObject obje)
+    {
+        obje.gameObject.SetActive(false);
+        objPool.Push(obje);
+    }
+}
