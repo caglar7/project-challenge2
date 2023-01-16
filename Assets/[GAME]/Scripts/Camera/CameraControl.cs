b74b19e25a2d9744940df51f60c3bb1eac264332
Caglar <mcaglarcnkr@gmail.com>
2023-01-16 05:34:52 +0300

    camera rotation around player when level done

---
+++ Assets/[GAME]/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+///  start rotation when level win event trigger
+/// </summary>
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] float lerpMult = 10f;
+    bool isRotating = false;
+    CinemachineVirtualCamera myCamera;
+
+    private void Awake()
+    {
+        myCamera = GetComponent<CinemachineVirtualCamera>();
+        EventManager.LevelWin += StartRotating;
+    }
+
+    private void Update()
+    {
+        if(isRotating)
+        {
+            Vector3 rot = transform.eulerAngles;
+            rot += new Vector3(0f, rotationSpeed * Time.deltaTime, 0f);
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rot, Time.deltaTime * lerpMult);
+        }
+    }
+
+    private void StartRotating()
+    {
+        ResetOffset();
+        isRotating = true;
+    }
+
+    private void StopRotating()
+    {
+        isRotating = false;
+    }
+
+    private void ResetOffset()
+    {
+        var transposer = myCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        transposer.m_TrackedObjectOffset = Vector3.zero;
+    }
+}
